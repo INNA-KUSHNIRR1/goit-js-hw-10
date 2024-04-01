0817a3726f65eb74b8f09e5864c0fcd69d06993c
INNA-KUSHNIRR1 <149315820+INNA-KUSHNIRR1@users.noreply.github.com>
2024-04-01 13:28:09 +0200

    fixed timer css

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,8 +1,7 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
-// Описаний у документації
+
 import iziToast from 'izitoast';
-// Додатковий імпорт стилів
 import 'izitoast/dist/css/iziToast.min.css';
 
 const ref = {
