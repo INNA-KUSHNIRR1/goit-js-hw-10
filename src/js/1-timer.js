e5f9fae9ed8d818bf139f4bd23f5f72d6b607e2e
INNA-KUSHNIRR1 <149315820+INNA-KUSHNIRR1@users.noreply.github.com>
2024-04-01 12:51:53 +0200

    done timer

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,13 +1,90 @@
 import flatpickr from 'flatpickr';
-
 import 'flatpickr/dist/flatpickr.min.css';
+// Описаний у документації
+import iziToast from 'izitoast';
+// Додатковий імпорт стилів
+import 'izitoast/dist/css/iziToast.min.css';
 
-flatpickr('#datetime-picker', {
+const ref = {
+  currentDays: document.querySelector('span[data-days]'),
+  currentHours: document.querySelector('span[data-hours]'),
+  currentMinutes: document.querySelector('span[data-minutes]'),
+  currentSeconds: document.querySelector('span[data-seconds]'),
+  btnStart: document.querySelector('button[data-start]'),
+  dateTimePicker: document.querySelector('#datetime-picker'),
+};
+
+let userSelectedDate = null;
+let intervalId = null;
+let diffDate = null;
+
+ref.btnStart.setAttribute('disabled', 'disabled');
+
+ref.dateTimePicker.addEventListener('click', () => {
+  ref.btnStart.removeAttribute('disabled', 'disabled');
+});
+
+const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    if (selectedDates[0] <= new Date()) {
+      iziToast.show({
+        title: 'WARNING!',
+        message: 'Please choose a date in the future',
+        color: 'green',
+        position: 'topRight',
+      });
+
+      ref.btnStart.setAttribute('disabled', 'disabled');
+    } else {
+      ref.btnStart.removeAttribute('disabled', 'disabled');
+      userSelectedDate = selectedDates[0];
+    }
   },
+};
+
+flatpickr('#datetime-picker', options);
+
+ref.btnStart.addEventListener('click', () => {
+  ref.btnStart.setAttribute('disabled', 'disabled');
+  ref.dateTimePicker.setAttribute('disabled', 'disabled');
+  intervalId = setInterval(() => {
+    diffDate = userSelectedDate - Date.now();
+
+    const { days, hours, minutes, seconds } = convertMs(diffDate);
+    console.log(convertMs(diffDate));
+    function addLeadingZero(obg) {
+      if (diffDate <= 1000) {
+        clearInterval(intervalId);
+        iziToast.show({
+          title: 'Stop sleeping!!!',
+          message: 'Wake up! ',
+          color: 'red',
+          position: 'bottomCenter',
+        });
+      }
+      ref.currentDays.textContent = `${days}`.padStart(2, '0');
+      ref.currentHours.textContent = `${hours}`.padStart(2, '0');
+      ref.currentMinutes.textContent = `${minutes}`.padStart(2, '0');
+      ref.currentSeconds.textContent = `${seconds}`.padStart(2, '0');
+    }
+    addLeadingZero({ days, hours, minutes, seconds });
+  }, 1000);
 });
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
